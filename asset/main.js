e585e388ef763899e766613241966571c780174b
StefanoNesi <sn.boolean@gmail.com>
2021-10-11 13:45:15 +0200

    Prompt question added

--- asset/main.js
+++ asset/main.js
@@ -1,8 +1,11 @@
 //Ask for distance
+const distance = parseInt(prompt("How many KMs you want to travel?"));
 
 //Ask for age
+const age = parseInt(prompt("How old are you?"));
 
 //Math 
+
 
 //Print Price
 
